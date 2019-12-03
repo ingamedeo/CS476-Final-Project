781373ae58688db228c014ac1538f2304bf0e83b
Amedeo Baragiola <ingamedeo@gmail.com>
2019-12-03 12:06:59 -0600

    mem2reg: look for stores in label_block

--- mem2reg.ml
+++ mem2reg.ml
@@ -111,7 +111,7 @@ let rec enable_mem2reg body fn_map offset rm_map label_block =
 	)
     | [] -> []
 
-Printf.printf "Starting mem2reg LLVM optimization. Nananana..\n"
+(* LLVM mem2reg optimization entry point *)
 let ssa_enabled =
         match main with
         | Function (fn_type, name, body) -> (

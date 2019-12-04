f74c79d9d1d908f893766e84163476e785a81ffc
Amedeo <ingamedeo@gmail.com>
2019-12-04 17:54:55 -0600

    added inline src with pragma

---
+++ sample_code/inline/inline-enabled.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__attribute__((always_inline))
+void change_value_to(float *dst){
+    *dst = rand();
+}
+
+float *change_and_return(float *dst){
+    *dst = rand();
+    return dst;
+}
+
+/* __attribute__((always_inline))
+inline void change_value_to2(float *dst);
+ */
+int main(void){
+
+    float a = 3.0;
+    change_value_to(&a);
+    printf("a is now %f\n", a);
+
+}
